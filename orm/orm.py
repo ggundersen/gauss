598cba6b1fb92b8190cb7e13d12fb6d45928cf4e
Gregory Gundersen <ggundersen@gmail.com>
2013-08-27 00:17:58 -0400

    massive overhaul of Python style

--- orm/orm.py
+++ orm/orm.py
@@ -1,36 +1,45 @@
-from problem import Problem
 from google.appengine.ext import ndb
+from problem import Problem
+
 
 ANCESTOR_KEY = ndb.Key('All', '1')
 
+
 class Orm:
 
-	def get_problems(self, title=False):
-		return Problem.get_sorted_problems(ANCESTOR_KEY)
+    def get_problems(self, title=False):
+
+        return Problem.get_sorted_problems(ANCESTOR_KEY, title)
+
 
 	def flush_datastore(self):
-		problems = Problem.get_ndb_problems(ANCESTOR_KEY)
-		for p in problems:
-			p.key.delete()
 
-	@classmethod
-	def rebuild_datastore(cls):
-		data = cls.get_canonical_data()
-		for p in data:
-			problem = Problem(number=p[0], answer=p[1], title=p[2], parent=ANCESTOR_KEY)
-			problem.put()
+        problems = Problem.get_ndb_problems(ANCESTOR_KEY)
+        for p in problems:
+            p.key.delete()
+
+
+    @classmethod
+    def rebuild_datastore(cls):
+
+        data = cls.get_canonical_data()
+        for p in data:
+            problem = Problem(number=p[0], answer=p[1], title=p[2], parent=ANCESTOR_KEY)
+            problem.put()
+
 
 	@classmethod
 	def get_canonical_data(cls):
-		return [\
-			[1, 233168, 'Multiples of 3 and 5'],\
-			[2, 4613732, 'Even Fibonacci numbers'],\
-			[3, 6857, 'Largest prime factor'],\
-			[4, 906609, 'Largest palindrome product'],\
-			[5, 232792560, 'Smallest multiple'],\
-			[6, 25164150, 'Sum square difference'],\
-			[7, 104743, '10001st prime'],\
-			[8, 40824, 'Largest product in a series'],\
-			[9, 31875000, 'Special Pythagorean triplet'],\
-			[10, 142913828922, 'Summation of primes']\
-		]
+
+        return [\
+            [1, 233168, 'Multiples of 3 and 5'],\
+            [2, 4613732, 'Even Fibonacci numbers'],\
+            [3, 6857, 'Largest prime factor'],\
+            [4, 906609, 'Largest palindrome product'],\
+            [5, 232792560, 'Smallest multiple'],\
+            [6, 25164150, 'Sum square difference'],\
+            [7, 104743, '10001st prime'],\
+            [8, 40824, 'Largest product in a series'],\
+            [9, 31875000, 'Special Pythagorean triplet'],\
+            [10, 142913828922, 'Summation of primes']\
+        ]
--- orm/problem.py
+++ orm/problem.py
@@ -1,26 +1,32 @@
 from google.appengine.ext import ndb
 import operator
 
+
 class Problem(ndb.Model):
 
-	number = ndb.IntegerProperty()
-	answer = ndb.IntegerProperty()
-	title = ndb.StringProperty()
+    number = ndb.IntegerProperty()
+    answer = ndb.IntegerProperty()
+    title = ndb.StringProperty()
 
-	@classmethod
-	def get_ndb_problems(cls, ancestor_key):
-		return cls.query(ancestor=ancestor_key)
 
-	@classmethod
-	def get_sorted_problems(cls, ancestor_key, title=False):
-		output = []
-		problems = cls.query(ancestor=ancestor_key)
-		if title == False:
-			for problem in problems:
-				output.append((problem.number, problem.answer))
-		else:
-			for problem in problems:
-				# call str() on problem.title because it is a unicode string
-				output.append((problem.number, problem.answer, str(problem.title)))		
-		
-		return sorted(output, key=lambda x: x[0])
+    @classmethod
+    def get_ndb_problems(cls, ancestor_key):
+    	
+        return cls.query(ancestor=ancestor_key)
+
+
+    @classmethod
+    def get_sorted_problems(cls, ancestor_key, title=False):
+
+        output = []
+        problems = cls.query(ancestor=ancestor_key)
+        if title == False:
+        	for problem in problems:
+        		output.append((problem.number, problem.answer))
+        else:
+        	for problem in problems:
+        	    # call str() on problem.title because it is a unicode string
+        	    output.append(
+        	    	(problem.number, problem.answer, str(problem.title))
+        	    )
+        	return sorted(output, key=lambda x: x[0])
